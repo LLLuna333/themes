7e4fe6b9285ad64e48a76612467118b7a178d3ae
Elena Gladkikh <programmer_elena@list.ru>
2025-09-21 01:06:49 +0500

    files added

---
+++ scriprs.js/themes.js
@@ -0,0 +1,29 @@
+let darkThemeButton = document.querySelector('.theme-button-dark');
+let lightThemeButton = document.querySelector('.theme-button-light');
+let serifFontButton = document.querySelector('.font-button-serif');
+let sansSerifFontButton = document.querySelector('.font-button-sans-serif');
+//включение темной темы
+darkThemeButton.onclick = function () {
+    document.body.classList.add('dark');
+    darkThemeButton.classList.add('active');
+    lightThemeButton.classList.remove('active');
+};
+//включение светлой темы
+lightThemeButton.onclick = function () {
+    document.body.classList.remove('dark');
+    lightThemeButton.classList.add('active');
+    darkThemeButton.classList.remove('active');
+};
+
+// обработчик включения шрифта с засечками 
+serifFontButton.onclick = function () {
+    document.body.classList.add('serif');
+    serifFontButton.classList.add('active');
+    sansSerifFontButton.classList.remove('active');
+}
+// код переключения шрифта без засечек
+sansSerifFontButton.onclick = function () {
+    document.body.classList.remove('serif');
+    sansSerifFontButton.classList.add('active');
+    serifFontButton.classList.remove('active');
+};
